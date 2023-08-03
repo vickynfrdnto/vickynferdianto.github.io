e62bebb83320f765aa356c01435b7a0827277e00
Vicky Nanda Ferdianto <132178127+vickynfrdnto@users.noreply.github.com>
2023-08-04 00:37:45 +0700

    Portfolio
    portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Jr FrontEnd Web Developer" , "Jr Graphic Designer" , "Jr System Analyst" , "Jr System Support" , "Database" , "Technical Support"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
